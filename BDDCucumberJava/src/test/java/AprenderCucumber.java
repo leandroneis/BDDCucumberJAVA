a731d8ad110830e9f9cb2352d3c64d634394683d
Leandro Neis <lgn200@gmail.com>
2025-01-09 21:58:07 -0300

    Aula 06 - Cenario Inicial

---
+++ BDDCucumberJava/src/test/java/AprenderCucumber.java
@@ -0,0 +1,21 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AprenderCucumber {
+
+    @Given("que criei o arquivo corretamente")
+    public void que_criei_o_arquivo_corretamente() {
+
+    }
+
+    @When("executá-lo")
+    public void executá_lo() {
+
+    }
+
+    @Then("a especificação deve finalizar com sucesso")
+    public void a_especificação_deve_finalizar_com_sucesso() {
+
+    }
+}
