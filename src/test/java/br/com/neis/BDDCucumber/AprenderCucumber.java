b81ec05a02a367b0ab4bc9d98498b21b0ba68cde
Leandro Neis <lgn200@gmail.com>
2025-01-15 18:03:36 -0300

    8. Integrando com JUnit

--- src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
+++ src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
@@ -1,23 +1,22 @@
 package br.com.neis.BDDCucumber;
 
-
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.Entao;
-import io.cucumber.java.pt.Quando;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 
-    @Dado("que criei o arquivo corretamente")
+    @Dado("^que criei o arquivo corretamente$")
     public void que_criei_o_arquivo_corretamente() {
         // Implementation here
     }
 
-    @Quando("executá-lo")
+    @Quando("^executá-lo$")
     public void executa_lo() {
         // Implementation here
     }
 
-    @Entao("a especificação deve finalizar com sucesso")
+    @Entao("^a especificação deve finalizar com sucesso$")
     public void a_especificacao_deve_finalizar_com_sucesso() {
         // Implementation here
     }
---
+++ src/test/java/br/com/neis/BDDCucumber/Runner.java
@@ -0,0 +1,7 @@
+package br.com.neis.BDDCucumber;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class Runner {
+}
