b0613e385e37bde97bf273f0c7b9a29fdb8199ff
Leandro Neis <lgn200@gmail.com>
2025-01-15 17:43:29 -0300

    6. Cenário Inicial

---
+++ src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
@@ -0,0 +1,24 @@
+package br.com.neis.BDDCucumber;
+
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+
+    @Dado("que criei o arquivo corretamente")
+    public void que_criei_o_arquivo_corretamente() {
+        // Implementation here
+    }
+
+    @Quando("executá-lo")
+    public void executa_lo() {
+        // Implementation here
+    }
+
+    @Entao("a especificação deve finalizar com sucesso")
+    public void a_especificacao_deve_finalizar_com_sucesso() {
+        // Implementation here
+    }
+}
