6736390edcbbb30338612c8974d5bc7a205bb787
Leandro Neis <lgn200@gmail.com>
2025-01-15 23:05:09 -0300

    11.Asserts

--- src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
+++ src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
@@ -1,10 +1,13 @@
 package br.com.neis.BDDCucumber;
 
+import org.junit.Assert;
+
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
+
     @Dado("^que criei o arquivo corretamente$")
     public void queCrieiOArquivoCorretamente() throws Throwable {
         System.out.println("qualquer coisa");
@@ -35,9 +38,7 @@ public class AprenderCucumber {
 
     @Então("^o valor do contador será (\\d+)$")
     public void oValorDoContadorSerá(int arg1) throws Throwable {
-        System.out.println(arg1);
-        System.out.println(contador);
+        Assert.assertEquals(arg1, contador);
     }
-
 
 }
