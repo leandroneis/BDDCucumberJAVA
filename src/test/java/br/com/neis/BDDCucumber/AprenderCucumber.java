869cc700e0a5bec73f36f922a0ac2c1a03fb3e27
Leandro Neis <lgn200@gmail.com>
2025-01-20 21:58:29 -0300

    12. Reaproveitando cenários - REGEX

--- src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
+++ src/test/java/br/com/neis/BDDCucumber/AprenderCucumber.java
@@ -1,5 +1,10 @@
 package br.com.neis.BDDCucumber;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.java.pt.Dado;
@@ -8,37 +13,71 @@ import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 
-    @Dado("^que criei o arquivo corretamente$")
-    public void queCrieiOArquivoCorretamente() throws Throwable {
-        System.out.println("qualquer coisa");
+	@Dado("^que criei o arquivo corretamente$")
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+		System.out.println("qualquer coisa");
 
-    }
+	}
 
-    @Quando("^executá-lo$")
-    public void executáLo() throws Throwable {
+	@Quando("^executá-lo$")
+	public void executáLo() throws Throwable {
 
-    }
+	}
 
-    @Então("^a especificação deve finalizar com sucesso$")
-    public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
 
-    }
+	}
 
-    private int contador = 0;
+	private int contador = 0;
 
-    @Dado("^que o valor do contador é (\\d+)$")
-    public void queOValorDoContadorÉ(int arg1) throws Throwable {
-        contador = arg1;
-    }
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+		contador = arg1;
+	}
 
-    @Quando("^eu incrementar em (\\d+)$")
-    public void euIncrementarEm(int arg1) throws Throwable {
-        contador = contador + arg1;
-    }
+	@Quando("^eu incrementar em (\\d+)$")
+	public void euIncrementarEm(int arg1) throws Throwable {
+		contador = contador + arg1;
+	}
 
-    @Então("^o valor do contador será (\\d+)$")
-    public void oValorDoContadorSerá(int arg1) throws Throwable {
-        Assert.assertEquals(arg1, contador);
-    }
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+		Assert.assertEquals(arg1, contador);
+	}
+
+	Date entrega = new Date();
+	
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarDias(int arg1,String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		} 
+		
+		if(tempo.equals("meses")){
+			cal.add(Calendar.MONTH, arg1);
+		}
+		
+		entrega = cal.getTime();
+		
+	}
+
+	@Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})")
+	public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data,dataFormatada);
+	}
 
 }
--- src/test/java/br/com/neis/BDDCucumber/aprender_cucumber.feature
+++ src/test/java/br/com/neis/BDDCucumber/aprender_cucumber.feature
@@ -13,3 +13,19 @@ Funcionalidade: Aprender Cucumber
     Dado que o valor do contador é 15
     Quando eu incrementar em 3
     Então o valor do contador será 18
+
+  Cenário: Deve incrementar contador
+    Dado que o valor do contador é 123
+    Quando eu incrementar em 35
+    Então o valor do contador será 158
+    
+  Cenário: Deve calcular atraso no prazo de entrega
+    Dado que a entrega é dia 05/04/2018
+    Quando a entrega atrasar 2 dias
+    Então a entrega será efetuada em 07/04/2018
+    
+  Cenário: Deve calcular atraso no prazo de entrega da china
+    Dado que a entrega é dia 05/04/2018
+    Quando a entrega atrasar 2 meses
+    Então a entrega será efetuada em 05/06/2018    
+    
